06ab403a6400c35f46deb67f29df36008c609085
Max Sevenig <max.sevenig@hotmail.com>
2022-04-06 23:06:43 +0200

    Created components needed for the categories list and moved styles to different files

--- src/App.js
+++ src/App.js
@@ -1,42 +1,37 @@
-import './categories.styles.scss'
+import CategoriesList from './components/categories-list/categories-list.component'
 
 const App = () => {
 
   const categories = [
     {
-      title: 'Hats',
-      id: 1
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png"
     },
     {
-      title: 'Jackets',
-      id: 2
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png"
     },
     {
-      title: 'Sneakers',
-      id: 3
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png"
     },
     {
-      title: 'Womens',
-      id: 4
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png"
     },
     {
-      title: 'Mens',
-      id: 5
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png"
     }
   ]
 
   return (
-    <div className='categories-container'>
-      {categories.map(({ title }) => (
-        <div className='category-container'>
-        <div className='background-image' />
-          <div className='category-body-container'>
-            <h2>{title}</h2>
-            <p>Shop now</p>
-          </div>
-        </div>
-      ))}
-    </div>
+      <CategoriesList categories={categories}/>
   );
 }
 
---
+++ src/components/categories-list/categories-list.component.jsx
@@ -0,0 +1,14 @@
+import './categories-list.styles.scss'
+import CategoryItem from '../category-item/category-item.component' 
+
+const CategoriesList = ({categories}) => {
+  return (
+    <div className='categories-list-container'>
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+    ))}
+    </div>
+  )
+}
+
+export default CategoriesList 
